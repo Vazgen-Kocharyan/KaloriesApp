00198626081f61d75d8d95a7b49f7b304da7297a
fgonzalezdiaz <fgonzalezd24042005@gmail.com>
2025-12-06 16:34:57 +0100

    Layouts acabats

--- app/src/main/java/com/example/myapplication/Configuration.kt
+++ app/src/main/java/com/example/myapplication/Configuration.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ContactUs.newInstance] factory method to
+ * Use the [Configuration.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ContactUs : Fragment() {
+class Configuration : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class ContactUs : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_us, container, false)
+        return inflater.inflate(R.layout.fragment_configuration, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class ContactUs : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactUs.
+         * @return A new instance of fragment Configuration.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ContactUs().apply {
+            Configuration().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
